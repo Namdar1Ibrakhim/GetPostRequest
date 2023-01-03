0086b5b6eb6917377da6f81089b41fbe6f7b07b1
Ibrakhim <113717353+Namdar1Ibrakhim@users.noreply.github.com>
2023-01-03 21:48:04 +0500

    Add files via upload

---
+++ GetZaprosApplication.java
@@ -0,0 +1,13 @@
+package com.example.getzapros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetZaprosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetZaprosApplication.class, args);
+    }
+
+}
